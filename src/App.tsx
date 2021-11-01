cea22bb6f18056e174b7afc4b61bd8d68de04c0a
Marketa Krucka <marketakrucka@gmail.com>
2021-11-01 17:47:51 +0100

    showing rate static

--- src/App.tsx
+++ src/App.tsx
@@ -22,17 +22,62 @@ const getExchangeRate = async (
 };
 
 const App = (): JSX.Element => {
-  // const [crypto, setCrypto] = useState('');
-  // const [currency, setCurrency] = useState('');
+  const getExchangeRate = async (
+    crypto: string,
+    currency: string
+  ): Promise<void> => {
+    try {
+      const response = await fetch(
+        `https://api.coingecko.com/api/v3/simple/price?ids=${crypto}&vs_currencies=${currency}`
+      );
+      const data = await response.json();
+      console.log(data);
+      setExchangeRateList(data);
+    } catch (err) {
+      console.error('rejected', err);
+    }
+  };
+
+  const [exchangeRateList, setExchangeRateList] =
+    useState<CurrencyPrice | null>(null);
+  const [from, setFrom] = useState('usd');
+  const [to, setTo] = useState('bitcoin');
 
   useEffect(() => {
-    getExchangeRate('bitcoin,ethereum', 'czk,gbp,usd');
+    getExchangeRate(
+      'bitcoin,ethereum,tether,cardano,solana,polkadot,ripple,usd-coin,chainlink,vechain',
+      'czk,gbp,usd,eur,cad,cny,jpy'
+    );
+    console.log(exchangeRateList);
   }, []);
 
   return (
     <div className="App">
       <header>
-        <p>Crypto converter</p>
+        <form>
+          <input type="number" />
+          <select name="crypto" id="">
+            <option value={from}>gbp</option>
+            <option value={to}>usd</option>
+          </select>
+          <br />
+          <input type="number" disabled />
+          <select name="crypto" id="">
+            <option value="bitcoin">BTC</option>
+            <option value="cardano">Cardano</option>
+          </select>
+        </form>
+        <h3>Exchange rate</h3>
+        {exchangeRateList !== null && exchangeRateList !== undefined ? (
+          <p>
+            {new Intl.NumberFormat('en-US', {
+              style: 'currency',
+              currency: 'usd',
+            }).format(exchangeRateList[to][from])}
+          </p>
+        ) : (
+          <p>Loading...</p>
+        )}
       </header>
     </div>
   );
